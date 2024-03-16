58a2e01a6ee7b4738be350b4dae92777c7b6d8de
BohdanB <wersall25@gmail.com>
2024-03-16 13:42:29 +0200

    add checking if docker is running and checking where to run the file. Fixed mounting of the directory in the container.

--- start.sh
+++ start.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-
+if ! docker ps &> /dev/null; then
+        echo "Docker не запущений, буде запущенно"
+        sudo systemctl start docker
+        sleep 4
+fi
+if [[ "$PWD" != *"docker-one"* ]]; then
+    # Перехід в папку docker-one
+    cd docker-one
+    echo -e "
+    щоб зупинити контейнери перейдіть в папку docker-one та
+     вконайте команду docker-compose down.
+    Це може виглядати так:
+    cd docker-one && docker-compose down\n"
+fi
 docker-compose up -d
 sleep 5
 docker ps
-echo "check 5 containers should be running."
+echo -e "\ncheck 5 containers should be running."
